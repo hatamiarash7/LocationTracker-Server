fbd183535d68623944259dfa43e700e58b412359
hatamiarash7 <hatamiarash7@gmail.com>
2019-01-02 15:55:43 +0330

    complete auth0 course

---
+++ backend/src/index.js
@@ -0,0 +1,106 @@
+//import dependencies
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const jwt = require('express-jwt');
+const jwksRsa = require('jwks-rsa');
+
+// define the Express app
+const app = express();
+
+// the database
+const questions = [
+    {
+        "id": 0,
+        "title": "How do I make a sandwich ?",
+        "description": "I am trying very hard, but I do not know how to make a delicious sandwich. Can someone help me ?",
+        "answers": []
+    }, {
+        "id": 1,
+        "title": "What is React ?",
+        "description": "I have been hearing a lot about React. What is it ?",
+        "answers": []
+    }
+];
+
+// enhance your app security with Helmet
+app.use(helmet());
+
+// use bodyParser to parse application/json content-type
+app.use(bodyParser.json());
+
+// enable all CORS requests
+app.use(cors());
+
+// log HTTP requests
+app.use(morgan('combined'));
+
+// retrieve all questions
+app.get('/', (req, res) => {
+    const qs = questions.map(q => ({
+        id: q.id,
+        title: q.title,
+        description: q.description,
+        answers: q.answers.length,
+    }));
+    res.send(qs);
+});
+
+// get a specific question
+app.get('/:id', (req, res) => {
+    const question = questions.filter(q => (q.id === parseInt(req.params.id)));
+    if (question.length > 1) return res.status(500).send();
+    if (question.length === 0) return res.status(404).send();
+    res.send(question[0]);
+});
+
+
+const checkJwt = jwt({
+    secret: jwksRsa.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: `https://ateriad.eu.auth0.com/.well-known/jwks.json`
+    }),
+
+    // Validate the audience and the issuer.
+    audience: 't4nKiXg631UAqaWMzRzDrDM6redd0Jo4',
+    issuer: `https://ateriad.eu.auth0.com/`,
+    algorithms: ['RS256']
+});
+
+app.post('/', checkJwt, (req, res) => {
+    const {title, description} = req.body;
+    const newQuestion = {
+        id: questions.length + 1,
+        title,
+        description,
+        answers: [],
+        author: req.user.name,
+    };
+    questions.push(newQuestion);
+    res.status(200).send();
+});
+
+// insert a new answer to a question
+app.post('/answer/:id', checkJwt, (req, res) => {
+    const {answer} = req.body;
+
+    const question = questions.filter(q => (q.id === parseInt(req.params.id)));
+    if (question.length > 1) return res.status(500).send();
+    if (question.length === 0) return res.status(404).send();
+
+    question[0].answers.push({
+        answer,
+        author: req.user.name,
+    });
+
+    res.status(200).send();
+});
+
+// start the server
+app.listen(8081, () => {
+    console.log('listening on port 8081');
+});
